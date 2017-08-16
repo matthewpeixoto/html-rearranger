22057118403c3ed693d1063929b819b008ac1375
Matthew Peixoto <matthewpeixoto@gmail.com>
2017-08-16 15:26:57 -0400

    Create headings and lists

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var $belowHeading = $('<h2>Below ground veggies</h2>');
+var $aboveHeading = $('<h2>Above ground veggies</h2>');
+var $belowList = $('<ul>');
+var $aboveList = $('<ul>');
+
+$('body')
+.append($belowHeading)
+.append($belowList)
+.append($aboveHeading)
+.append($aboveList);
